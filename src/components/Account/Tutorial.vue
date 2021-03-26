6cde36e7dcacc2f758c57b3027c3a9125b859024
ynagxingqian <18256048243@163.com>
2021-03-26 23:51:32 +0800

    tutorial

--- src/components/Account/Tutorial.vue
+++ src/components/Account/Tutorial.vue
@@ -1,2 +1,172 @@
-<template>  <div class="AboutUs paddingtop-50 padding-20">    <NavTit :title="title" :noTit="false"></NavTit>    <div class="contxt">      <h3 class="fontweight-m text-center margintop-20 marginbottom-10 fontsize16">How to buy USDT and sell USDT on UMAP for profit？</h3>      <p>UMAP helps you Sell USDT for Fiat directly with other Buyers. It's 24x7, safe and absolutely legal!</p>      <p>simple things you can do with UMAP:</p>      <p>Cash Out - If you want to move Fiat to your bank account, Sell your cryptos for USDT and then sell that USDT for Fiat via UMAP!</p>      <p>Broadly, here's how UMAP P2P works:</p>      <p>- UMAP matches the person looking to buy USDT for Fiat with someone who’s looking to sell USDT for Fiat</p>      <p>- UMAP escrows the USDT for safekeeping during the transaction</p>      <p>- Buyer transfers Fiat to the seller's bank account via IMPS/UPI</p>      <p>- Seller confirms payment and UMAP releases the USDT to the buyer</p>      <p>Is UMAP safe?</p>      <p>        Absolutely! UMAP has an escrow system to make the entire transaction secure so neither party can cheat the other. In the escrow system, UMAP will hold the seller's USDT        until the transaction is successfully completed and the payment is confirmed.      </p>      <p>If you’re a seller - UMAP will not release your USDT to the buyer until you confirm the receipt of payment.</p>      <p>        You can buy USDT on Bitbns (https://bitbns.com), and all other exchanges that support USDT crypto and make money by transferring to Umap via TRC20. We offer a 2.5% premium        based on the real time price of the official exchange.      </p>      <h4>How to buy USDT in Bitbns</h4>      <p>        Bitbns website:        <a class="a_style" href="https://bitbns.com">https://bitbns.com</a>      </p>      <p>        Bitbns download link:        <a class="a_style" href="https://play.google.com/store/apps/details?id=com.bitbns">https://play.google.com/store/apps/details?id=com.bitbns</a>      </p>    </div>  </div></template><script>import NavTit from '../NavAndTit.vue';export default {  name: 'PrivacyPolicy',  components: {    NavTit  },  data() {    return {      title: 'Privacy & Policy'    };  },  created() {    this.$store.state.loading = false;  },  mounted() {},  methods: {}};</script><style lang="scss" scoped>.contxt {  padding-bottom: 40px;  h3 {    line-height: 28px;    padding: 0 50px;  }  h4 {    font-weight: 600;    margin-top: 10px;    font-size: 15px;    line-height: 26px;  }  h5 {    font-weight: 600;    line-height: 24px;    margin-top: 8px;  }  p {    line-height: 22px;    margin-top: 4px;    .a_style {      color: #19bc9c;
-      text-decoration: underline;    }  }}</style>
+<template>
+	<div class="AboutUs paddingtop-50 padding-20">
+		<NavTit :noTit="true"></NavTit>
+		<div class="contxt">
+			<h3 class="fontweight-m text-center margintop-20 marginbottom-10 fontsize16">How to buy USDT and sell USDT on UMAP for profit？</h3>
+			<p>UMAP helps you Sell USDT for Fiat directly with other Buyers. It's 24x7, safe and absolutely legal!</p>
+			<p>simple things you can do with UMAP:</p>
+			<p>Cash Out - If you want to move Fiat to your bank account, Sell your cryptos for USDT and then sell that USDT for Fiat via UMAP!</p>
+			<p>Broadly, here's how UMAP P2P works:</p>
+			<p>- UMAP matches the person looking to buy USDT for Fiat with someone who’s looking to sell USDT for Fiat</p>
+			<p>- UMAP escrows the USDT for safekeeping during the transaction</p>
+			<p>- Buyer transfers Fiat to the seller's bank account via IMPS/UPI</p>
+			<p>- Seller confirms payment and UMAP releases the USDT to the buyer</p>
+			<p>Is UMAP safe?</p>
+			<p>
+				Absolutely! UMAP has an escrow system to make the entire transaction secure so neither party can cheat the other. In the escrow system, UMAP will hold the seller's USDT until the transaction is successfully completed and the payment is confirmed.
+			</p>
+			<p>If you’re a seller - UMAP will not release your USDT to the buyer until you confirm the receipt of payment.</p>
+			<p>
+				You can buy USDT on Bitbns (https://bitbns.com), and all other exchanges that support USDT crypto and make money by transferring to Umap via TRC20. We offer a 2.5% premium based on the real time price of the official exchange.
+			</p>
+			<h4>How to buy USDT in Bitbns</h4>
+			<p>
+				Bitbns website:
+				<a class="a_style" href="https://bitbns.com">https://bitbns.com</a>
+			</p>
+			<p>
+				Bitbns download link:
+				<a class="a_style" href="https://play.google.com/store/apps/details?id=com.bitbns">https://play.google.com/store/apps/details?id=com.bitbns</a>
+			</p>
+			<p class="fontweight-m">1. Select the recharge method according to your needs and transfer the rupee to the bitbns balance. </p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/1.png" />
+			</div>
+			<p class="fontweight-m">2. Search USDT</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/2.png" />
+				<img src="../../assets/img/tutorial/3.png" />
+			</div>
+			<p class="fontweight-m">3. Buy USDT</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/4.png" />
+			</div>
+			<p class="fontweight-m">4. Enter the purchase quantity</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/5.png" />
+			</div>
+			<p class="fontweight-m">5. Pay the bill</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/6.png" />
+			</div>
+			<p class="fontweight-m">6. Check whether the USDT has arrived</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/7.png" />
+			</div>
+			<p class="fontweight-m">7. Congratulations, you have bought USDT from Bitbns successfully.the next step is sell USDT on UMAP for profit.</p>
+			<h4>UMAP sells USDT for profit</h4>
+			<p>UMAP download link:
+				<a href="http://sell.umap.vip/apk/umap-release.apk">http://sell.umap.vip/apk/umap-release.apk</a>
+			</p>
+			<p>UMAP website:<a href="http://sell.umap.vip">http://sell.umap.vip</a></p>
+			<p class="fontweight-m">1. Open UMAP in app or website, click sell, select the quantity to sell, sell USDT</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/8.png" />
+				<img src="../../assets/img/tutorial/9.png" />
+			</div>
+			<p class="fontweight-m">2. Copy address <b style="color: #FF003F">（Due to a bug in bitbns, you can only transfer money by copying the address, you cannot scan the code to transfer USDT）</b></p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/10.png" />
+			</div>
+			<p class="fontweight-m">3. Log in to bitbns app，Click wallet,Close hide small bal,Search USDT,Click send .</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/11.png" />
+			</div>
+			<p class="fontweight-m">4. Select TRC20, copy UMAP's wallet address, and enter the quantity to be sold.</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/12.png" />
+			</div>
+			<p class="fontweight-m">5. Copy the Hash data, save the screenshot of the successful payment of USDT and the wallet address of the transfer.</p>
+			<div class="imgWrap">
+				<img style="width: 100%;" src="../../assets/img/tutorial/13.png" />
+			</div>
+			<p class="fontweight-m">6. Fill in the outgoing address just copied, fill in the Hash value into the TxID, and choose your own payment method, finally upload a screenshot of the payment, and submit it for confirmation.</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/14.png" />
+			</div>
+			<p class="fontweight-m">7. Waiting for order review</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/15.png" />
+			</div>
+			<p class="fontweight-m">8. If the customer purchases your order, UMAP will send a text message to remind you whether to receive the money and confirm the order</p>
+			<div class="imgWrap">
+				<img style="width: 100%;" src="../../assets/img/tutorial/16.png" />
+			</div>
+			<p class="fontweight-m">9. Money arrives in the bank account.</p>
+			<div class="imgWrap">
+				<img src="../../assets/img/tutorial/17.png" />
+			</div>
+			<p>Congratulations, you have successfully sold the USDT at a profit.You can keep trading and make more money.</p>
+			<p>Note: If you have any questions, you can join our official trading group: </p>
+			<p>Telegram: <a class="a_style3" href="https://t.me/joinchat/I76Batq1XdFxqxdo">https://t.me/joinchat/I76Batq1XdFxqxdo</a></p>
+			<p>Whatsapp: <a class="a_style3" href="http://wa.me/919319609962">http://wa.me/919319609962</a></p>
+		</div>
+	</div>
+</template>
+
+<script>
+	import NavTit from '../NavAndTit.vue';
+	export default {
+		name: 'PrivacyPolicy',
+		components: {
+			NavTit
+		},
+		data() {
+			return {};
+		},
+		created() {
+			this.$store.state.loading = false;
+		},
+		mounted() {},
+		methods: {}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.contxt {
+		padding-bottom: 40px;
+		h3 {
+			line-height: 28px;
+			padding: 0 50px;
+		}
+		h4 {
+			font-weight: 600;
+			margin-top: 30px;
+			font-size: 15px;
+			line-height: 26px;
+		}
+		h5 {
+			font-weight: 600;
+			line-height: 24px;
+			margin-top: 8px;
+		}
+		p {
+			line-height: 22px;
+			margin-top: 4px;
+			a {
+				text-decoration: underline;
+				&.a_style {
+					color: #19bc9c;
+				}
+				&.a_style2 {
+					color: #6D4FFD;
+				}
+				&.a_style3 {
+					color: #6D4FFD;
+				}
+			}
+			b{
+				font-weight: 600;
+			}
+		}
+		.imgWrap {
+			width: 100%;
+			display: flex;
+			margin: 30px 0;
+			justify-content: center;
+			img {
+				width: 170px;
+			}
+		}
+	}
+</style>
